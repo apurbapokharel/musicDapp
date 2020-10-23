840adf4626e60a70c0aa2f0caf9afdd717ddab64
apurbapokharel <pokharel.apurba@gmail.com>
2020-10-23 12:01:47 +0545

    Stored music from BC in redux

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,13 +2,13 @@
 // export const ADD_SONG = 'ADD_SONG';
 // export const VIEW_SONG = 'VIEW_SONG';
 
-// export const ADD_SONGS = (value) => {
-//     console.log(value);
-//     return{
-//         type:"ASSIGNMENT",
-//         payload: value
-//     }
-// };
+export const ADD_SONGS = (value) => {
+    console.log(value);
+    return{
+        type:"ADD_SONGS_ASSIGNMENT",
+        payload: value
+    }
+};
 
 export const ADD_MUSIC_CONTRACT = (value) => {
     return{
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux'
-// import musicAssigner from './musicReducer'
+import musicAssigner from './musicReducer'
 import musicContrackAssigner from './musicContrackReducer'
 import tokenContractAssigner from './tokenReducer'
 import tokenSaleContractAssigner from './tokenSaleReducer'
@@ -12,7 +12,7 @@ import orbitReducer from './orbitDbReducer'
 import musicReducer from "./musicDbReducer";
 
 const allReducers = combineReducers({
-    // musics: musicAssigner,
+    musics: musicAssigner,
     musicContract: musicContrackAssigner,
     tokenContract: tokenContractAssigner,
     tokenSaleContract: tokenSaleContractAssigner,
